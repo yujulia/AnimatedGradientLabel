6d2465538fcef64803687fb8db4949ca3e158b9a
Julia Yu <juliayu@gmail.com>
2017-03-31 15:00:06 -0700

    add test with attributed text

--- AnimatedGradientLabel/AnimatedGradientLabel/ViewController.swift
+++ AnimatedGradientLabel/AnimatedGradientLabel/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: AnimatedGradientLabel!
+    @IBOutlet weak var secondLabel: AnimatedGradientLabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let shadow = NSShadow()
+        shadow.shadowBlurRadius = 3
+        shadow.shadowOffset = CGSize(width: 3, height: 3)
+        shadow.shadowColor = UIColor.gray
+        let shadowStringText = "Shadow String"
+        let shadowAttribute = [ NSShadowAttributeName: shadow ]
+        let shadowString = NSAttributedString(string: shadowStringText, attributes: shadowAttribute)
+
+
+        let underlineStringText = "Underline String"
+        let underlineAttribute = [ NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue ]
+        let underlineString = NSAttributedString(string: underlineStringText, attributes: underlineAttribute)
+
+        executeAfter(3) {
+            self.label.attributedText = shadowString
+        }
+
+        executeAfter(9) {
+            self.label.attributedText = underlineString
+        }
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+public func executeAfter(_ delay: Double, queue: DispatchQueue? = nil, closure: @escaping () -> Void) {
+    let time = DispatchTime.now() + DispatchTimeInterval.milliseconds(Int(delay * 1000))
+    (queue ?? DispatchQueue.main).asyncAfter(deadline: time, execute: closure)
+}
